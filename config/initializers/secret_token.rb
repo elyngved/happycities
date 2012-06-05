8e5c1537f3f34b6d8b18860270605ac43e3cf6dc
Erik Lyngved <elyngved@gmail.com>
2012-06-05 14:22:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happycities::Application.config.secret_token = '0e4370311e81538012118508d51c05fdfff741932e202250c3fd7428bc6cbee1485bcc6cb6de8625e196b2f767a6bd3626a9440ddf4b7ad9cfbd78d89b4d96bd'
